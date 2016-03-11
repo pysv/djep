58ac793b31237fc74fdf05e4f55f5d9a10c8df62
Max Brauer <max@transcode.de>
2016-03-11 11:52:37 +0100

    Fixed navigation for mobile.

--- pyconde/skins/euroscipy2016/static/assets/js/main.js
+++ pyconde/skins/euroscipy2016/static/assets/js/main.js
@@ -1,9 +1,4 @@
-$(document).ready(function() {
-
-  var windowHeight = $(window).height();
-  var footerBottom = $('#footer').offset().top + $('#footer').height();
-
- if (footerBottom < windowHeight) {
-    $('#footer').css('margin-top', '-39px');
-  }
+$(document).ready(function () {
+    var minHeight = $(window).height() - $('header').height() - $('footer').height();
+    $('#main').css('min-height', minHeight + 'px');
 });
