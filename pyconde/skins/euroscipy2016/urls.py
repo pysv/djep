58e8c68485678a432e490859a0c82fab4059f1b4
Max Brauer <max@transcode.de>
2016-04-17 19:20:18 +0200

    Tried another urlconf pattern.

--- pyconde/skins/euroscipy2016/urls.py
+++ pyconde/skins/euroscipy2016/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import include, url
 
 urlpatterns = [
-    url(r'^2016test/', include('pyconde.urls')),
-    url(r'^2016/', include('pyconde.urls')),
+    url(r'^2016test/$', include('pyconde.urls')),
 ]
