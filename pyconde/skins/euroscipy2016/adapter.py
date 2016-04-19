171f190ae01d9e4fbb60e9a81823a68e0352c3a1
Max Brauer <max@transcode.de>
2016-04-19 18:01:55 +0200

    Fixed custom Adapter.

--- pyconde/skins/euroscipy2016/adapter.py
+++ pyconde/skins/euroscipy2016/adapter.py
@@ -5,10 +5,10 @@ from allauth.account.adapter import DefaultAccountAdapter
 class EuroSciPy2016Adapter(DefaultAccountAdapter):
 
     def get_login_redirect_url(self, request):
-        return settings.LOGIN_ERROR_URL
-
-    def get_logout_redirect_url(self, request):
         return settings.LOGIN_REDIRECT_URL
 
-    def get_email_confirmation_redirect_url(self, request):
+    def get_logout_redirect_url(self, request):
         return settings.LOGOUT_REDIRECT_URL
+
+    def get_email_confirmation_redirect_url(self, request):
+        return settings.LOGIN_REDIRECT_URL
