39048efa3496d7374ed12796333cda5f03ed2ed1
Max Brauer <max@transcode.de>
2016-05-08 19:56:36 +0200

    Reformatted urls of checkin app.

--- pyconde/checkin/urls.py
+++ pyconde/checkin/urls.py
@@ -10,9 +10,10 @@ urlpatterns = patterns(
     url(r'^purchase/$', 'purchase_view', name='checkin_purchase'),
     url(r'^purchase/(?P<pk>\d+)/$', 'purchase_detail_view', name='checkin_purchase_detail'),
     url(r'^purchase/(?P<pk>\d+)/badges/$', 'purchase_badges_view', name='checkin_purchase_badges'),
-    url(r'^purchase/(?P<pk>\d+)/invoice/$', 'purchase_invoice_view', name='checkin_purchase_invoice'),
-    url(r'^purchase/(?P<pk>\d+)/(?P<new_state>[^/]+)/$', 'purchase_update_state', name='checkin_purchase_state'),
-
+    url(r'^purchase/(?P<pk>\d+)/invoice/$', 'purchase_invoice_view',
+        name='checkin_purchase_invoice'),
+    url(r'^purchase/(?P<pk>\d+)/(?P<new_state>[^/]+)/$', 'purchase_update_state',
+        name='checkin_purchase_state'),
     url(r'^ticket/(?P<pk>\d+)/badge/$', 'ticket_badge_view', name='checkin_ticket_badge'),
     url(r'^ticket/(?P<pk>\d+)/edit/$', 'ticket_update_view', name='checkin_ticket_update'),
 
