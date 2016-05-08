2edbacab499da8f7c15bd7533759ba5cc3b664d3
Max Brauer <max@transcode.de>
2016-05-08 19:54:02 +0200

    Fixed paymill js.

--- pyconde/skins/euroscipy2016/static/assets/js/main.js
+++ pyconde/skins/euroscipy2016/static/assets/js/main.js
@@ -1,4 +1,21 @@
+jQuery.fn.preventDoubleSubmission = function() {
+    var last_clicked, time_since_clicked;
+    jQuery(this).bind('submit', function(event) {
+        if(last_clicked) {
+            time_since_clicked = jQuery.now() - last_clicked;
+        }
+        last_clicked = jQuery.now();
+        if(time_since_clicked < 2000) {
+            // console.log("Blocking form submit because it was too soon after the last submit.");
+            event.preventDefault();
+        }
+        // else console.log("submit");
+        return true;
+    });
+};
+
 $(document).ready(function () {
     var minHeight = $(window).height() - $('header').height() - $('footer').height();
     $('#main').css('min-height', minHeight + 'px');
+
 });
